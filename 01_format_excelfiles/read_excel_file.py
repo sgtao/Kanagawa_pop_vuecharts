e331eacb0aafb92f987e5983685658d36ea015d0
Shogo Ogami <sg.tao.so@gmail.com>
2021-09-18 14:16:26 +0900

    update 01_format.. for test of print data frame

--- 01_format_excelfiles/read_excel_file.py
+++ 01_format_excelfiles/read_excel_file.py
@@ -4,6 +4,22 @@ import pandas as pd
 import sys
 import os
 #
+#
+# print Data Sheet
+
+
+def print_data_sheet(input_sheet_df):
+  #DataFrameとしてsheet1枚のデータ(2019)を読込み
+  #読み込んだシートの先頭10行を表示
+  # print(vars(input_sheet_df))
+  # input_sheet_df.head(10)
+  # print(vars(input_sheet_df.head(10)))
+  # 行ごとに処理
+  for i, row in input_sheet_df.iterrows():
+      print(i, row)
+
+
+# read excel file
 def read_excel_file(input_file_name):
   # input_file_name = 'jinkotosetai_r3_8.xlsx'
   # df = pd.read_excel(input_file_name, index_col=0)
@@ -13,11 +29,10 @@ def read_excel_file(input_file_name):
   input_book = pd.ExcelFile(input_file_name)
   #sheet_namesメソッドでExcelブック内の各シートの名前をリストで取得できる
   input_sheet_name = input_book.sheet_names
-  #lenでシートの総数を確認
-  num_sheet = len(input_sheet_name)
-  #シートの数とシートの名前のリストの表示
-  print("Sheet の数:", num_sheet)
-  print(input_sheet_name)
+  #DataFrameとしてsheet1枚を読込み
+  input_sheet_df = input_book.parse(input_sheet_name[0], header=None)
+  # 読み込んだシートの表示
+  print_data_sheet(input_sheet_df)
 #
 # main routine
 if __name__ == '__main__':
