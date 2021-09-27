ff3cb779d872ade0917cc37ef82df9cd28217a5d
Shogo Ogami <sg.tao.so@gmail.com>
2021-09-28 00:19:35 +0900

    update 03_trial for add SideNavigator, setting router,store

---
+++ 03_trial_chartjs_pop/src/components/SideNav.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-navigation-drawer v-model="$store.state.drawer" absolute temporary>
+    <v-list>
+      <v-list-item>
+        <v-app-bar-nav-icon @click.stop="toggleSideMenu"></v-app-bar-nav-icon>
+        <v-list-item-content>
+          <v-list-item-title>Menu</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list-item v-for="(item, index) in items" :key="index" :to="item.link">
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer></template>
+<script>
+import { mapActions } from 'vuex'
+export default {
+  data () {
+    return {
+      // icon の参照情報は、"https://pictogrammers.github.io/@mdi/font/2.0.46/"を参照して選択
+      items: [
+        { title: 'ホーム', icon: 'mdi-home', link: { name: 'Home'} },
+        { title: 'サイトについて', icon: 'mdi-information-outline', link: { name: 'About' } }
+      ]
+    }
+  },
+  methods: {
+    ...mapActions(['toggleSideMenu'])
+  }
+}
+</script>
--- 03_trial_chartjs_pop/src/store/index.js
+++ 03_trial_chartjs_pop/src/store/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+//
 Vue.use(Vuex)
-
+//
 export default new Vuex.Store({
+  // ステートを定義する
   state: {
+    // ステート対象の初期値(false)を定義
+    drawer: false
   },
+  // ステートの状態を更新する
   mutations: {
+    // アクションの内容として、drawer状態を反転を定義する
+    toggleSideMenu(state) {
+      state.drawer = !state.drawer
+    }
   },
+  // actions はコンポーネントとのインタフェース
   actions: {
-  },
-  modules: {
+    toggleSideMenu({ commit }) {
+      commit('toggleSideMenu')
+    }
   }
 })
