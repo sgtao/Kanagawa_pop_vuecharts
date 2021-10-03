8d87bd3ac076c1f3f3400d0dd04d8805707f9a68
Shogo Ogami <sg.tao.so@gmail.com>
2021-10-04 07:07:48 +0900

    update docs by build 03_trial..

--- 03_trial_chartjs_pop/src/views/About.vue
+++ 03_trial_chartjs_pop/src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <h1>about this page</h1>
+    <div>This page shows Population information of Kanagawa Prefecture.</div>
+    <main>
+      <div>Data source : <a href="https://www.pref.kanagawa.jp/docs/x6z/tc30/jinko/kohyosiryo.html" target="_blank">神奈川県人口統計調査（月報）</a></div>
+    </main>
+    <div>
+      <span>Contents source : <a href="https://github.com/sgtao/kanagawa_population_info" target="_blank">GitHub</a></span>、
+      <span>Copyright: 2021<a href="mailto:sg.tao.so@gmail.com">@sgtao</a></span>
+    </div>
   </div>
 </template>
+<style lang="scss">
+  .container {
+    display: block;
+    main {
+      width: 90%;
+      margin: 0 0 200px 0;
+    }
+  }
+</style>
+<script>
+  export default {
+    name: 'About',
+  }
+</script>
--- 03_trial_chartjs_pop/src/views/Home.vue
+++ 03_trial_chartjs_pop/src/views/Home.vue
@@ -1,15 +1,25 @@
 <template>
-  <hello-world />
+  <div class="container">
+    <h1>神奈川県内人口状況</h1>
+    <main>
+      <p>神奈川県の市町村人口状況を表示します。</p>
+      <p>表示形式を左上メニューからお選びください</p>
+    </main>
+  </div>
 </template>
-
+<style lang="scss">
+  .container {
+    display: block;
+    width: 90%;
+    margin: 0 auto;
+    main {
+      width: 90%;
+      margin: 0 auto;
+    }
+  }
+</style>
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
   export default {
     name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
   }
 </script>
